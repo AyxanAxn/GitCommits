3080aad39e344c71ca72ed78c47085616b3fd682
Axun_8bbn <Axun_8bbn@itstep.edu.az>
2022-03-05 14:48:05 +0400

    Add project files.

---
+++ GitCommits/FakeDb/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GitCommits.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitCommits.FakeDb
+{
+    public class AppDbContext
+    {
+        public static List<DataOfList> dataOfList = new List<DataOfList>() {
+        new DataOfList{ Cotnent="Salam canim",Title="Saba cixiram"},
+        };
+    }
+}

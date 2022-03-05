93b801d955279c0a7c9604bab0699bc92425f548
Axun_8bbn <Axun_8bbn@itstep.edu.az>
2022-03-05 15:24:50 +0400

    UpdateMetod

---
+++ GitCommits/DAL/UpdateDataService.cs
@@ -0,0 +1,23 @@
+﻿using GitCommits.FakeDb;
+using GitCommits.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitCommits.DAL
+{
+    public class UpdateDataService
+    {
+        public void updateData(DataOfList data)
+        {
+
+            var datas = AppDbContext.dataOfList.FirstOrDefault(d => d.Id == data.Id);
+            if (datas != null)
+            {
+                data.Title = datas.Title;
+                data.Cotnent = datas.Cotnent;
+            }
+        }
+    }
+}

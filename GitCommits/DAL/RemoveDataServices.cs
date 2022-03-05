6a611dfc4f2dbf1d20434a872228fc348146f9d3
Mammadov Sanan Shahin <Mamm_kk1o@itstep.edu.az>
2022-03-05 15:29:57 +0400

    Remove Method

--- GitCommits/DAL/RemoveDataServices.cs
+++ GitCommits/DAL/RemoveDataServices.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using GitCommits.FakeDb;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GitCommits.DAL
 {
-    public class Remove
+    public static class RemoveDataServices
     {
+        public static void RemoveData(int Id)
+        {
+            try
+            {
+                AppDbContext.DataOfLists.Remove(AppDbContext.DataOfLists.FirstOrDefault(x => x.Id == Id));
+            }
+            catch(Exception ex)
+            {
+                 
+
+            }
+        }
     }
 }
